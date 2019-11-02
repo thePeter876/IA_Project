efa0394a2770d8e7fbfee0c5fba6de3ae4cbe0b2
adrivp <adr.vaquero@gmail.com>
2019-11-02 18:47:17 +0100

    Desarrollo Adri QLearningMind 1

--- IA Q-Learning Unity/Assets/Scripts/SampleMind/QLearningMind.cs
+++ IA Q-Learning Unity/Assets/Scripts/SampleMind/QLearningMind.cs
@@ -5,6 +5,60 @@ namespace Assets.Scripts.SampleMind
 {
     public class QLearningMind : AbstractPathMind
     {
+        const int numEpisodios = 1000;
+        const int numAcciones = 4;
+        const float alfa = 0.5f;
+        const float gamma = 0.5f;
+        const int rMeta = 100;
+        const int rMuro = -10;
+        float[,,] tablaQ;
+
+        public bool ComprobadorFichero() 
+        {
+            return false;
+        }
+
+        public void AlgoritmoQ(BoardInfo boardInfo, CellInfo[] goals) 
+        {
+            tablaQ = new float[boardInfo.NumColumns, boardInfo.NumRows, numAcciones];
+            int[] posicion = new int[2];
+            int accion;
+
+            void ObtenerMaxQ(int[] pos)
+            {
+                float maxValue = float.MinValue;
+                //tablaQ[pos[0], pos[1], 0]
+            }
+
+            for(int epis = 0; epis < numEpisodios; epis++)
+            {
+                posicion[0] = Random.Range(0, boardInfo.NumColumns);
+                posicion[1] = Random.Range(0, boardInfo.NumRows);
+
+                while (boardInfo.CellInfos [posicion[0], posicion[1] ].Walkable && boardInfo.CellInfos[ posicion[0], posicion[1] ] != goals[0])
+                {
+                    accion = Random.Range(0, numAcciones);
+                    switch (accion)
+                    {
+                        case 0: // arriba
+                            posicion[1]++;
+                            break;
+                        case 1: // abajo
+                            posicion[1]--;
+                            break;
+                        case 2: // izquierda
+                            posicion[0]--;
+                            break;
+                        case 3: // derecha
+                            posicion[0]++;
+                            break;
+                    }
+
+                }
+
+            }
+        }
+
         public override void Repath()
         {
 
@@ -12,7 +66,14 @@ namespace Assets.Scripts.SampleMind
 
         public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
         {
-            // si tabla no existe crear, si existe leer
+            /*
+             * METER EN AWAKE
+             * 
+             * if (!ComprobadorFichero()) // IMPORTANTE programar la comprobación
+            {
+                AlgoritmoQ(boardInfo, goals);
+            }
+            */
 
             var val = Random.Range(0, 4);
             if (val == 0) return Locomotion.MoveDirection.Up;
@@ -20,10 +81,5 @@ namespace Assets.Scripts.SampleMind
             if (val == 2) return Locomotion.MoveDirection.Left;
             return Locomotion.MoveDirection.Right;
         }
-
-        // método crear tabla (PONERLE NOMBRE SEMILLA, ASÍ TENEMOS EL FICHERO CORRESPONDIENTE CREADO)
-
-        // método crear array con valores tabla, pillar ancho y alto de boardinfo
-
     }
 }

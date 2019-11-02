9f42c22e769cabfe873d1cc4946bca24757a773e
thePeter876 <38359059+thePeter876@users.noreply.github.com>
2019-11-02 12:04:13 +0100

    0.0
    -Actualizado proyecto a una versión de unity más actual
    -Creado script QLearningMind para trabajar en la práctica

---
+++ IA Q-Learning Unity/Assets/Scripts/SampleMind/QLearningMind.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.DataStructures;
+using UnityEngine;
+
+namespace Assets.Scripts.SampleMind
+{
+    public class QLearningMind : AbstractPathMind
+    {
+        public override void Repath()
+        {
+
+        }
+
+        public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
+        {
+            // si tabla no existe crear, si existe leer
+
+            var val = Random.Range(0, 4);
+            if (val == 0) return Locomotion.MoveDirection.Up;
+            if (val == 1) return Locomotion.MoveDirection.Down;
+            if (val == 2) return Locomotion.MoveDirection.Left;
+            return Locomotion.MoveDirection.Right;
+        }
+
+        // método crear tabla (PONERLE NOMBRE SEMILLA, ASÍ TENEMOS EL FICHERO CORRESPONDIENTE CREADO)
+
+        // método crear array con valores tabla, pillar ancho y alto de boardinfo
+
+    }
+}
